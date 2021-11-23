88ffd8a144b54e9337f82c5ee3ff5d9c937bc881
Saif <skh9588@g.rit.edu>
2021-11-23 11:20:22 +0400

    New function: train_model to clean up main function a bit
    New model added : XGB
    
    train/test variables made global to clean up functions

--- project.py
+++ project.py
@@ -5,6 +5,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LassoLars
 from sklearn.linear_model import Ridge
+from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import mean_squared_error
 
@@ -13,10 +14,14 @@ from sklearn.metrics import mean_squared_error
  Written by Saifeldin Hassan and Tony Hanna.
 '''
 
+
 def main():
+    """
+    Main function, visualizes the data and creates/tests the models used in this project
+    """
 
     df = load_data('mall_customers.csv')
-    x_train, y_train, x_test, y_test = train_model(df)
+    train_model(df)
 
     # Data visualization
 
@@ -34,30 +39,40 @@ def main():
 
     # Linear regression
     linear_regression_model = LinearRegression()
-    linear_regression_model.fit(x_train, y_train)
-    linear_regression_prediction = linear_regression_model.predict(x_test)
-
+    linear_regression_prediction = model_test("Linear Regression", linear_regression_model)
     evaluate(y_test, linear_regression_prediction, "Linear Regression")
-    plot_scatter('Linear Regression model', 'Linear Regression', y_test, linear_regression_prediction)
+    plot_scatter('Linear Regression model', 'Linear Regression',  linear_regression_prediction)
 
     # Least angle regression
     lars_lasso_model = LassoLars(alpha=.1, normalize=False)
-    lars_lasso_model.fit(x_train, y_train)
-    lars_lasso_prediction = lars_lasso_model.predict(x_test)
-
-    plot_scatter("LARS LASO", "Least angle regression", y_test, lars_lasso_prediction)
+    lars_lasso_prediction = model_test("LARS LASSO", lars_lasso_model)
+    plot_scatter("LARS LASO", "Least angle regression",  lars_lasso_prediction)
     evaluate(y_test, lars_lasso_prediction, "LARS LASO")
 
     # Ridge regression model
 
     ridge_regression_model = Ridge(alpha=.5)
-    ridge_regression_model.fit(x_train, y_train)
-    ridge_regression_prediction = ridge_regression_model.predict(x_test)
+    ridge_regression_prediction = model_test("Ridge Regression", ridge_regression_model)
 
-    plot_scatter("Ridge regression", "Ridge regression", y_test, ridge_regression_prediction)
+    plot_scatter("Ridge regression", "Ridge regression", ridge_regression_prediction)
     evaluate(y_test, ridge_regression_prediction, "Ridge Regression")
 
+    # XGB Regressor
+
+    xgb_regressor_model = XGBRegressor()
+    xgb_regressor_prediction = model_test("XGB Regressor", xgb_regressor_model)
+
+    plot_scatter("XGB", "XGB Regressor", xgb_regressor_prediction)
+    evaluate(y_test, xgb_regressor_prediction, "XGB")
+
     plt.show()
+
+
+def model_test(model_name, model):
+    model.fit(x_train, y_train)
+    model_prediction = model.predict(x_test)
+
+    return model_prediction
 
 
 def load_data(filename):
@@ -78,14 +93,13 @@ def train_model(df):
     """
     Function preps model training variables
     """
+    global x_train, y_train, x_test, y_test
     # Create the test train split
     train, test = train_test_split(df, test_size=0.4)
     x_train = train[['Gender', 'Age', 'Annual_Income']]
     y_train = train['Spending_Score_100']
     x_test = test[['Gender', 'Age', 'Annual_Income']]
     y_test = test['Spending_Score_100']
-
-    return x_train, y_train, x_test, y_test
 
 
 def evaluate(actual, prediction, name):
@@ -98,12 +112,12 @@ def evaluate(actual, prediction, name):
     print('\n' + f'L1 Loss ( {name} )= {l1_loss:.2f}' + '\n'f'L2 Loss ( {name} ) = {l2_loss:.2f}')
 
 
-def plot_scatter(figure, title, x_plot, y_plot):
+def plot_scatter(figure, title, prediction):
     """
     To make main function cleaner, shortened scatter plot function
     """
     plt.figure(figure)
-    sns.scatterplot(x=x_plot, y=y_plot)
+    sns.scatterplot(x=y_test, y=prediction)
     plt.title(title)
     plt.xlabel('Real')
     plt.ylabel('Prediction')
